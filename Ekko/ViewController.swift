dc04de825c085488904068146873b6b2f5de2dd7
Aravind Sundaresan <sundrsn2@illinois.edu>
2015-06-27 14:03:00 -0700

    Initial Commit

---
+++ Ekko/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ekko
+//
+//  Created by Aravind Sundaresan on 6/27/15.
+//  Copyright © 2015 GreylockBetas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
