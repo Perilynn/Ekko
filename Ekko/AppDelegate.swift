7e0d22cd83a24090fd314476468a2b5ccc07ec8d
Aravind Sundaresan <sundrsn2@illinois.edu>
2015-06-28 01:55:16 -0700

    completely working

--- Ekko/AppDelegate.swift
+++ Ekko/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   let responder = EkkoResponder()
   var state: MousePositionState = .Exited
   @IBOutlet weak var window: NSWindow!
+  @IBOutlet weak var statusMenu: NSMenu!
+  let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+  
+  var myRef = Firebase(url:"https://greylock-ekko.firebaseio.com/ios/urls/")
 
   func applicationDidFinishLaunching(aNotification: NSNotification) {
     window.styleMask = NSBorderlessWindowMask
@@ -22,6 +26,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     window.backgroundColor = NSColor.grayColor()
     window.alphaValue = 0.01
     window.level = Int(CGWindowLevelForKey(CGWindowLevelKey.MaximumWindowLevelKey))
+    let icon = NSImage(named: "MenuIcon")
+    statusItem.image = icon
+    statusItem.menu = statusMenu
+    authWithPassword()
+    hasBumped("https://www.google.com") //will replace with input string from AppleScript
+    
+    
     
     NSEvent.addGlobalMonitorForEventsMatchingMask(NSEventMask.LeftMouseDraggedMask) { (event: NSEvent) -> Void in
       if NSScreen.mainScreen()!.frame.width - event.locationInWindow.x < 35 {
@@ -55,6 +66,45 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
   func applicationWillTerminate(aNotification: NSNotification) {
     // Insert code here to tear down your application
+  }
+  
+  func authWithPassword() {
+    myRef.authUser("sundrsn2@illinois.edu", password: "ekko1",
+      withCompletionBlock: {error, authData in
+        
+        if error != nil {
+          //error logging in
+        }
+        else {
+          //now logged in
+        }
+    });
+    firebaseListener()
+  }
+  
+  func authWithGitHub() {
+    
+    
+  }
+  
+  func hasBumped(url: String) {
+    // Write data to Firebase
+    //    var myLink = ["application": "Google Chrome", "url": "https://www.google.com"]
+    //    var yourLink = ["application": "Firefox", "url": "https://www.facebook.com"]
+    //    var usersRef = myRef.childByAppendingPath("users")
+    //
+    //    var users = ["Aravind": myLink, "Naren": yourLink]
+    //    usersRef.setValue(users)
+    var link = ["name": "Google Chrome", "url": url]
+    myRef.updateChildValues(link)
+  }
+  
+  func firebaseListener() {
+    myRef.observeEventType(.Value, withBlock: { snapshot in
+      print(snapshot.value.objectForKey("url")!)
+      }, withCancelBlock: { error in
+        print(error.description)
+    })
   }
 }
 
